8806ee79ab0dcf4b5d13424b5bb6b6b08d436f54
Saulius Gražulis <grazulis@ibt.lt>
2018-11-19 18:12:48 +0200

    Deleting the 'grammars/filters.ebnf' which is no longer needed.
    All tests now use the file generated from the documentation,
    'generated/Filter.ebnf'.

--- tests/cases/ebnf_filters_504.sh
+++ tests/cases/ebnf_filters_504.sh
@@ -3,6 +3,12 @@
 # Test case: test if a provided Filter langauge EBNF grammar correctly
 # recognises filter expressions
 
+#BEGIN DEPEND
+
+INPUT_GRAMMAR=generated/Filter.ebnf
+
+#END DEPEND
+
 ./tools/grammatiker/BNF/scripts/grammatica-tree \
-    <(./tools/grammatiker/EBNF/scripts/ebnf2grammatica grammars/filters.ebnf) \
+    <(./tools/grammatiker/EBNF/scripts/ebnf2grammatica ${INPUT_GRAMMAR}) \
     <(echo -n filter='((NOT (a>b)) AND x>0)')
